a1268e8a0c1b253b92a418f1e45df0ff4508ca70
Kiwix <kiwix.shell@gmail.com>
2024-11-28 09:20:10 +0100

    Update Morpion.c ; Add est_fini function

--- morpion.c
+++ morpion.c
@@ -8,14 +8,13 @@
 
 struct morpion{
 	int qui_le_tour;
-	int fini;
 	char grille[N][N];
 	int gagnant;
 };
 
 void affichage(struct morpion grille); // Affiche la grille
 struct morpion tour_joueur(struct morpion grille); // Fonction qui gère l'intéraction et modifie la grille entre les 2 joueurs
-struct morpion est_fini(struct morpion grille); // Fonction pour analyser si la partie est fini après chaque tour
+int est_fini(struct morpion grille); // Fonction pour analyser si la partie est fini après chaque tour
 
 /* Fonction Principale */
 int main()
@@ -23,17 +22,14 @@ int main()
 
 
 	char tab_grille[N][N] = {
-	{' ', ' ', ' '},
-	{' ', ' ', ' '},
-	{' ', ' ', ' '}
+	{'a', '-', '-'},
+	{'-', '-', '-'},
+	{'-', '-', '-'}
 	};
 
 	struct morpion grille = {P1, 0, tab_grille[3][3], 0};
 
-	// Boucle Principale qui fait tourner le jeu.
-	while (!grille.fini){
-		tour_joueur(grille);
-	}
+	tour_joueur(grille);
 
 	return EXIT_SUCCESS;
 }
@@ -50,63 +46,67 @@ void affichage(struct morpion grille) {
 
 
 struct morpion tour_joueur(struct morpion grille){
-	
+	int fini = 0;
 	int x; /* x ou le joueur va jouer */ 
 	int y; /* y ou le joueur va jouer */ 
 
-	while(!grille.fini){
-
+	while(fini == 0){
+		fini = est_fini(grille);
 		if (grille.qui_le_tour == P1){
-			printf("Joueur 1 c'est à vous de jouer\nOu voulez vous jouer ? : ");		
-			scanf("%d %d", &x , &y); /* recuperer les valeurs */ 
-		       	grille.grille[x][y] = 'x'; /* ajoute x dans le tableau au bon endroit */ 
-			affichage(grille); 
-		        est_fini(grille);	
-			grille.qui_le_tour = P2;
+			if (fini == 0){
+				printf("Joueur 1 c'est à vous de jouer\nOu voulez vous jouer ? : ");		
+				scanf("%d %d", &x , &y); /* recuperer les valeurs */ 
+		       		grille.grille[x][y] = 'x'; /* ajoute x dans le tableau au bon endroit */ 
+				affichage(grille); 	
+				grille.qui_le_tour = P2;
+			}
 		}
 
 		else if (grille.qui_le_tour == P2){
-			printf("Joueur 2 c'est à vous de jouer\nOu voulez vous jouer ? : ");
-			scanf("%d %d", &x, &y); 
-			grille.grille[x][y] = 'o';
-			affichage(grille);
-			est_fini(grille);
-			grille.qui_le_tour = P1; 
+			if (fini == 0){
+				printf("Joueur 2 c'est à vous de jouer\nOu voulez vous jouer ? : ");
+				scanf("%d %d", &x, &y); 
+				grille.grille[x][y] = 'o';
+				affichage(grille); 
+				grille.qui_le_tour = P1; 
+			}
 		}
-
 	}
 }
 
-struct morpion est_fini(struct morpion grille){
+int est_fini(struct morpion grille){
 
-	// Condition qui arrête les recherches si on a trouvé un gagnant.
-	if (grille.gagnant == 0){
+	for (int i = 0; i<3; i++){
+		// Gère les lignes
+		if (grille.grille[i][0] == 'x' && (grille.grille[i][1] == 'x' && grille.grille[i][2] == 'x'))
+				grille.gagnant = P1;
+	        if (grille.grille[i][0] == 'o' && (grille.grille[i][1] == 'o' && grille.grille[i][2] == 'o'))
+				grille.gagnant = P2;
 		
-		for (int i = 0; i<3; i++){
-			// Test pour les lignes
-			if (grille.grille[i][0] == 'X' && grille.grille[i][1] == 'X' && grille.grille[i][2] == 'X')
+		// Gère les colonnes
+		if (grille.grille[0][i] == 'x' && (grille.grille[1][i] == 'x' && grille.grille[2][i] == 'x'))
 				grille.gagnant = P1;
-			if (grille.grille[i][0] == 'O' && grille.grille[i][1] == 'O' && grille.grille[i][2] == 'O')
+		if (grille.grille[0][i] == 'o' && (grille.grille[1][i] == 'o' && grille.grille[2][i] == 'o'))
+				grille.gagnant = P2;
+		
+		// Gère les diagonales
+		if (grille.grille[0][0] == 'x' && (grille.grille[1][1] == 'x' && grille.grille[2][2] == 'x'))
+				grille.gagnant = P1;
+		if (grille.grille[0][0] == 'o' && (grille.grille[1][1] == 'o' && grille.grille[2][2] == 'o'))
 				grille.gagnant = P2;
 
-			// Test pour les colonnes
-			if (grille.grille[0][i] == 'X' && grille.grille[1][i] == 'X' && grille.grille[2][i] == 'X')
+                if (grille.grille[0][2] == 'x' && (grille.grille[1][1] == 'x' && grille.grille[2][0] == 'x'))
 				grille.gagnant = P1;
-			if (grille.grille[0][i] == 'O' && grille.grille[1][i] == 'O' && grille.grille[2][i] == 'O')
-				grille.gagnant = P2;
 
-			// Test pour les 2 diagonales
-			if (grille.grille[0][0] == 'X' && grille.grille[1][1] == 'X' && grille.grille[2][2] == 'X')
+                if (grille.grille[0][2] == 'o' && (grille.grille[1][1] == 'o' && grille.grille[2][0] == 'o'))
 				grille.gagnant = P1;
-			if (grille.grille[0][0] == 'O' && grille.grille[1][1] == 'O' && grille.grille[2][2] == 'O')
-				grille.gagnant = P2;
-			
-			if (grille.grille[0][2] == 'X' && grille.grille[1][1] == 'X' && grille.grille[2][0] == 'X')
-				grille.gagnant = P1;
-			if (grille.grille[0][2] == 'O' && grille.grille[1][1] == 'O' && grille.grille[2][0] == 'O')
-				grille.gagnant = P2;
-		}
-	}		
-	return grille;
+	}
+
+	// Arrête le jeu
+	if (grille.gagnant != 0){
+	        printf("Le joueur %d à gagné, bien joué !\n", grille.gagnant);
+		return 1;
+	} else {
+		return 0;
+	}
 }
-
